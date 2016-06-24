737f6bc4a9f40b6da40d4694d3e52445a38d4330
esme518 <esme518@gmail.com>
2016-06-25 05:49:04 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -59,7 +59,7 @@ if [ ! -f /etc/ocserv/server-key.pem ] || [ ! -f /etc/ocserv/server-cert.pem ]; 
 	
 	# add cn-no-route
 	if [ -z "$NO_CN_ROUTE" ]; then
-		curl -o /tmp/route.txt https://raw.githubusercontent.com/CNMan/ocserv-cn-no-route/master/cn-no-route.txt
+		curl -SL "https://raw.githubusercontent.com/CNMan/ocserv-cn-no-route/master/cn-no-route.txt" -o /tmp/route.txt
 		sed -i '/route = /d' /etc/ocserv/ocserv.conf
 		echo '# ocserv-cn-no-route' > /etc/ocserv/ocserv.conf
 		cat /tmp/route.txt >> /etc/ocserv/ocserv.conf
